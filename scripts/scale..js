2ef19e37c53d48eb2ee118047f30bda9e22aa97e
bora <nbora617@naver.com>
2023-07-28 18:10:09 +0900

    main

---
+++ scripts/scale..js
@@ -0,0 +1,8 @@
+//변수생성
+const height = document.querySelector('#dietFrm #height');  //키
+const weight = document.querySelector('#dietFrm #weight'); //몸무게
+const age = document.querySelector('#dietFrm #age')  //나이
+const properWeight =document.querySelector('.result_box .weight_data')  //적정체중
+const overWeight = document.querySelector('.result_box .over_weight_data')  //초과체중
+
+console.log(height,weight,age,properWeight,overWeight)
